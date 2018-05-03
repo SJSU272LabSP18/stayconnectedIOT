681d4cb770fbfbf5cf1050cbf639e967e1cb5bc9
Faisal Rahman <mizan_mohd2@hotmail.com>
2018-05-02 23:08:23 -0700

    UI updates

--- server/client/src/components/Dashboard/index.js
+++ server/client/src/components/Dashboard/index.js
@@ -1,51 +1,32 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Zones from './Zones/Zones';
-import LocationView from './Location';
-import Sites from './Sites/Sites';
-import Nodes from './Nodes/Nodes';
-import Locations from './Locations/Locations';
-import Navbar from '../Navbar/Navbar';
-import Header from '../Header';
+import React, {Component} from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import Zones from "./Zones/Zones";
+import Sites from "./Sites/Sites";
+import Nodes from "./Nodes/Nodes";
+import Locations from "./Locations/Locations";
+import Navbar from "../Navbar/Navbar";
 class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <div>
-            <div className="wrapper">
-              <div className="main-panel">
-                <Navbar />
-                <Route exact path="/dashboard/sites" component={Sites} />
-                <Route exact path="/dashboard/zones" component={Zones} />
-                <Route
-                  exact
-                  path="/dashboard/locations/:locationId/zones"
-                  component={Zones}
-                />
-                <Route
-                  exact
-                  path="/dashboard/:siteId/locations"
-                  component={Locations}
-                />
-                <Route
-                  exact
-                  path="/dashboard/locations"
-                  component={Locations}
-                />
-                <Route
-                  exact
-                  path="/dashboard/zones/:zoneId/nodes"
-                  component={Nodes}
-                />
-                <Route exact path="/dashboard/nodes" component={Nodes} />
-              </div>
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <div className="wrapper">
+                            <div className="main-panel">
+                                <Navbar />
+                                <Route exact path="/dashboard/sites" component={Sites}/>
+                                <Route exact path="/dashboard/zones" component={Zones}/>
+                                <Route exact path="/dashboard/locations/:locationId/zones" component={Zones}/>
+                                <Route exact path="/dashboard/:siteId/locations" component={Locations}/>
+                                <Route exact path="/dashboard/locations" component={Locations}/>
+                                <Route exact path="/dashboard/zones/:zoneId/nodes" component={Nodes}/>
+                                <Route exact path="/dashboard/nodes" component={Nodes}/>
+                            </div>
+                        </div>
+                    </div>
+                </BrowserRouter>
             </div>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
+        );
+    }
 }
 export default Dashboard;
