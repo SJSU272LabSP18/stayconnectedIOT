cafae4c022024b8aac4d63433b3b7b1e98e08074
Vera Wang <verawang37@gmail.com>
2018-05-09 16:13:44 -0700

    query specific date from the user or return general station info

--- server/routes/noaaRoutes.js
+++ server/routes/noaaRoutes.js
@@ -1,10 +1,11 @@
 var request = require('request');
 var express = require('express');
 var router = express.Router();
+var token = 'EhzyTdGyXfiqTVYCoPXfWmBxlFSGVLof';
 
 
 router.get('/', (req, res) => {
-  var condition = '&datatypeid=TOBS&startdate=2010-05-01&enddate=2010-05-01&units=metric';
+  var condition = '&datatypeid=TOBS&startdate=2000-01-01&units=metric';
   var config = {
     url: 'https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?datasetid=GHCND' + condition,
     headers: {
@@ -21,28 +22,44 @@ router.get('/', (req, res) => {
 
 router.get('/:stationId', (req, res) => {
   var stationId = req.params.stationId;
-  var condition = '&datatypeid=TOBS&startdate=2010-05-01&enddate=2010-05-01&units=metric';
-  var station_cond = '&stationid=' + stationId;
-  var config = {
-    url: 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND' + station_cond + condition,
-    headers: {
-      'Token': 'EhzyTdGyXfiqTVYCoPXfWmBxlFSGVLof'
-    }
-  };
+  var startdate = req.query.date;
+  console.log(startdate);
+  if(startdate == null){
+    var config = {
+      url: 'https://www.ncdc.noaa.gov/cdo-web/api/v2/stations/' + stationId,
+      headers: {
+        'Token': token
+      }
+    };
+
+  } else{
+    var condition = '&datatypeid=TOBS&startdate='+startdate+'&enddate='+startdate+'&units=metric';
+    var station_cond = '&stationid=' + stationId;
+    var config = {
+      url: 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND' + station_cond + condition,
+      headers: {
+        'Token': token
+      }
+    };
+  }
+
   request(config, function (error, response, body) {
     //var result = body.results[0].value;
     console.log(JSON.stringify(body));
     var result = JSON.parse(body);
     var data;
-    if(Object.keys(result).length === 0 && result.constructor === Object){
-      res.status(401).send({message: 'Error: Station has no data'});
+    if(error) {
+      res.status(401).send({message: 'Error getting data'});
     }else {
-      data = result.results[0];
-      res.status(200).send(data);
+      if(Object.keys(result).length === 0 && result.constructor === Object){
+        res.send({message: 'Station has no data on this date'});
+      }else {
+        res.status(200).send(body);
+      }
     }
-
   });
 });
+
 
 
 module.exports = router;
